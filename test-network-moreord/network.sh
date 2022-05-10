b0000d29f22ab54252a82bfcc6997aa267cb7f3b
Nicola <nicola.salvadore@gmail.com>
2022-05-10 22:19:43 +0200

    add orderer2

--- test-network-moreord/network.sh
+++ test-network-moreord/network.sh
@@ -182,37 +182,37 @@ function createOrgs() {
   fi
 
   # Create crypto material using Fabric CA
-  if [ "$CRYPTO" == "Certificate Authorities" ]; then
-    infoln "Generating certificates using Fabric CA"
-    ${CONTAINER_CLI_COMPOSE} -f compose/$COMPOSE_FILE_CA -f compose/$CONTAINER_CLI/${CONTAINER_CLI}-$COMPOSE_FILE_CA up -d 2>&1
+#   if [ "$CRYPTO" == "Certificate Authorities" ]; then
+#     infoln "Generating certificates using Fabric CA"
+#     ${CONTAINER_CLI_COMPOSE} -f compose/$COMPOSE_FILE_CA -f compose/$CONTAINER_CLI/${CONTAINER_CLI}-$COMPOSE_FILE_CA up -d 2>&1
 
-    . organizations/fabric-ca/registerEnroll.sh
+#     . organizations/fabric-ca/registerEnroll.sh
 
-    while :
-    do
-      if [ ! -f "organizations/fabric-ca/org1/tls-cert.pem" ]; then
-        sleep 1
-      else
-        break
-      fi
-    done
+#     while :
+#     do
+#       if [ ! -f "organizations/fabric-ca/org1/tls-cert.pem" ]; then
+#         sleep 1
+#       else
+#         break
+#       fi
+#     done
 
-    infoln "Creating Org1 Identities"
+#     infoln "Creating Org1 Identities"
 
-    createOrg1
+#     createOrg1
 
-    infoln "Creating Org2 Identities"
+#     infoln "Creating Org2 Identities"
 
-    createOrg2
+#     createOrg2
 
-    infoln "Creating Orderer Org Identities"
+#     infoln "Creating Orderer Org Identities"
 
-    createOrderer
+#     createOrderer
 
-  fi
+#   fi
 
-  infoln "Generating CCP files for Org1 and Org2"
-  ./organizations/ccp-generate.sh
+#   infoln "Generating CCP files for Org1 and Org2"
+#   ./organizations/ccp-generate.sh
 }
 
 # Once you create the organization crypto material, you need to create the
