fa53c11dfd0284b54ebabc56780a9c5518610781
Nicola <nicola.salvadore@gmail.com>
2022-05-28 20:30:06 +0200

    script with results

--- runbenchmarks.sh
+++ runbenchmarks.sh
@@ -4,6 +4,7 @@ export CHAINCODE=false
 export RUN=false
 export CRASH=false
 export NETWORK=0
+export ALL=false
 
 function printHelp(){
 	echo "Aiuto!"
@@ -32,6 +33,9 @@ while [[ $# -ge 1 ]] ; do
   -s )
     RUN=true
     ;;
+  --all )
+	ALL=true
+    ;;
   * )
     errorln "Unknown flag: $key"
     printHelp
@@ -41,41 +45,79 @@ while [[ $# -ge 1 ]] ; do
   shift
 done
 
-if [ "$NETWORK" == "0" ]
-then
-	echo "You must specify the -n option"
-	printHelp
-	exit 1
+function runTest()
+{
+	$NETWORK=$1
+	if [ "$NETWORK" == "0" ]
+	then
+		echo "You must specify the -n option"
+		printHelp
+		exit 1
+	else
+		cd ./test-network-scale-${NETWORK}x 
+	fi
+
+	if $RUN == true
+	then
+		echo "Running network with $NETWORK nodes"
+		./network.sh up createChannel
+	fi
+
+	if $CHAINCODE == true
+	then
+		echo "Installing test chaincode on network ${NETWORK}"
+		./network.sh deployCC -ccn fabcar -ccp ../caliper-benchmarks/src/fabric/samples/fabcar/go -ccl go
+	fi
+
+	if $CRASH == true
+	then
+		echo "Starting faulty benchmark tests on network ${NETWORK}"
+		./ordererRandomCrash.sh &
+	else
+		echo "Starting non-faulty benchmark tests on network ${NETWORK}"
+	fi
+	sed -i '' 's/test-network-scale-5x/test-network-scale-'${NETWORK}'x/' ../caliper-benchmarks/networks/fabric/test-network.yaml
+	sed -i '' 's/test-network-scale-10x/test-network-scale-'${NETWORK}'x/' ../caliper-benchmarks/networks/fabric/test-network.yaml
+	sed -i '' 's/test-network-scale-20x/test-network-scale-'${NETWORK}'x/' ../caliper-benchmarks/networks/fabric/test-network.yaml
+
+
+	cd ../caliper-benchmarks
+	npx caliper launch manager --caliper-workspace ./ --caliper-networkconfig networks/fabric/test-network.yaml --caliper-benchconfig benchmarks/samples/fabric/fabcar/config.yaml --caliper-flow-only-test --caliper-fabric-gateway-enabled
+
+	if $CRASH == true; then
+		cp report.html ../report-${NETWORK}-network-withFailure.html
+	else
+		cp report.html ../report-${NETWORK}-network-withoutFailure.html
+	fi
+}
+
+function runAllTests()
+{
+	NETWORKS=(5 10 20)
+	for net in ${NETWORKS[@]} ; do
+		echo "++++++++++++++++ RUN TEST WITH NETWORK ${net} WITHOUT FAULT +++++++++++++++++"
+		NETWORK=$net
+		# run the network, install the chaincode, run tests with crashes off
+		RUN=true
+		CHAINCODE=true
+		CRASH=false
+		runTest $net
+		# keep cold networks before next test
+		sleep 150
+		echo "++++++++++++++++ RUN TEST WITH NETWORK ${net} WITH FAULT +++++++++++++++++"
+		# opposite options of before, keep alive the network and run tests with crashes on
+		RUN=false
+		CHAINCODE=false
+		CRASH=true
+		runTest $net
+		# kill the network
+		cd ..
+		./test-network-scale-${NETWORK}x/network.sh down
+	done
+}
+
+if $ALL == true; then
+	runAllTests
 else
-	cd ./test-network-scale-${NETWORK}x 
+	runTest $NETWORK
 fi
-
-if $RUN == true
-then
-	echo "Running network with $NETWORK nodes"
-	./network.sh up createChannel
-fi
-
-if $CHAINCODE == true
-then
-	echo "Installing test chaincode on network ${NETWORK}"
-	./network.sh deployCC -ccn fabcar -ccp ../caliper-benchmarks/src/fabric/samples/fabcar/go -ccl go
-fi
-
-if $CRASH == true
-then
-	echo "Starting faulty benchmark tests on network ${NETWORK}"
-	./ordererRandomCrash.sh &
-else
-	echo "Starting non-faulty benchmark tests on network ${NETWORK}"
-fi
-sed -i '' 's/test-network-scale-5x/test-network-scale-'${NETWORK}'x/' ../caliper-benchmarks/networks/fabric/test-network.yaml
-sed -i '' 's/test-network-scale-10x/test-network-scale-'${NETWORK}'x/' ../caliper-benchmarks/networks/fabric/test-network.yaml
-sed -i '' 's/test-network-scale-20x/test-network-scale-'${NETWORK}'x/' ../caliper-benchmarks/networks/fabric/test-network.yaml
-
-
-cd ../caliper-benchmarks
-npx caliper launch manager --caliper-workspace ./ --caliper-networkconfig networks/fabric/test-network.yaml --caliper-benchconfig benchmarks/samples/fabric/fabcar/config.yaml --caliper-flow-only-test --caliper-fabric-gateway-enabled
-
-cp report.html ../report-${NETWORK}-network.html
-
