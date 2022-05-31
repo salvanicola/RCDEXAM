cb29d038932521ef8f24dad4a37576859603b1d4
TheDoctor200 <alex.thedoctor200@gmail.com>
2022-05-31 16:17:01 +0200

    Aggiunte info legate al monitor nei benchmarks

--- runbenchmarks.sh
+++ runbenchmarks.sh
@@ -160,11 +160,11 @@ function runAllTests()
 
 function scaleTest()
 {
-	for i in {1..10}
+	for i in {1..15}
 	do
 		changeTps $((i*10))
 		runTest
-		mv results/report-${NETWORK}-network-withoutFailure.html results/report-${NETWORK}-network-$$((i*10))tps.html
+		mv results/report-${NETWORK}-network-withoutFailure.html results/report-${NETWORK}-network-$((i*10))tps.html
 		CHAINCODE=false
 		RUN=false
 		sleep 60
